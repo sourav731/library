428a680c24034d2d0e37c7db22770928b9ca2ed8
Sourav Prasad <souravprasad731@gmail.com>
2020-03-30 01:13:05 +0530

    Card added in card status page

--- student/src/routes/cardStatus.js
+++ student/src/routes/cardStatus.js
@@ -9,15 +9,16 @@ function CardStatus(props) {
   return (
     <div
       style={{
-        backgroundColor: "#4A71B0",
-        display: "flex",
-        flexWrap: "wrap",
-        alignContent: "center"
+        backgroundColor: "#4A71B0"
       }}
     >
       <NavigationBar cardNumber="SL2039" />
+
+      <div style={{ paddingTop: "8%", textAlign: "center", color: "white" }}>
+        <h1>CARD STATUS</h1>
+      </div>
       <Container>
-        <Row style={{ paddingTop: "5%", paddingBottom: "2%" }}>
+        <Row style={{ paddingTop: "1%", paddingBottom: "2%" }}>
           <Col style={{ paddingTop: "5%" }}>
             <LibraryCard
               cardNumber="1"
@@ -59,7 +60,6 @@ function CardStatus(props) {
         </Row>
         <Row
           style={{
-            paddingLeft: "15%",
             paddingBottom: "5%"
           }}
         >
@@ -78,6 +78,19 @@ function CardStatus(props) {
           <Col style={{ paddingTop: "5%" }}>
             <LibraryCard
               cardNumber="5"
+              bookName="Concept of physics"
+              author="H.C.Verma"
+              issueDate="18.03.2020"
+              submissionDate="02.04.2020"
+              accNum="AC6549"
+              classNum="BA-8"
+              department="Information Tech"
+            />
+          </Col>
+
+          <Col style={{ paddingTop: "5%" }}>
+            <LibraryCard
+              cardNumber="6"
               bookName="Concept of physics"
               author="H.C.Verma"
               issueDate="18.03.2020"
